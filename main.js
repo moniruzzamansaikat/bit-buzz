292431e7ff551b544a47f2be85ade10f217ee005
Moniruzzman Saikat <saikat.moniruzzman@gmail.com>
2022-01-22 18:20:08 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+// aos init
+AOS.init();
